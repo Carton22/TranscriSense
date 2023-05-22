161d5b0f60e0a04bc3a5901ea169c8aa3d2f20e2
Carton Liu <1435742009@qq.com>
2023-05-22 16:32:12 +0800

    change content view

--- TranscriSense/ContentView.swift
+++ TranscriSense/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView{
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                Text("Welcome to TranscriSense!")
+                Image(systemName: "snowflake")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
         }
         .padding()
     }
--- TranscriSense/TranscriSenseApp.swift
+++ TranscriSense/TranscriSenseApp.swift
@@ -15,3 +15,5 @@ struct TranscriSenseApp: App {
         }
     }
 }
+
+
