2c9874923d70762328019c16670e9a27f525aec9
Carton Liu <1435742009@qq.com>
2023-05-22 15:39:28 +0800

    Initial Commit

---
+++ TranscriSense/TranscriSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriSenseApp.swift
+//  TranscriSense
+//
+//  Created by carton22 on 2023/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TranscriSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
